88356bf69a00e579baa6a470bed7ea97ea65d441
Gabriel Rodrigues Campos <huijirohankei@gmail.com>
2023-03-29 15:30:58 -0300

    Initial commit

---
+++ src/main/java/com/dci/drone_manager/DroneManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dci.drone_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneManagerApplication.class, args);
+	}
+
+}
